17cf0a9b89900a48d09902902cf8fd233ba695d1
Oleksandr Zozulia <freemanlex@gmail.com>
2023-12-11 21:09:01 +0200

    "first commit"

---
+++ verification/tests.py
@@ -0,0 +1,36 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ["Bengt Hilgursson"],
+            "answer": "Bongt Hulgirssen",
+        },
+        {
+            "input": ["Why do you laugh? I chose death."],
+            "answer": "Why da yee loigh? U chasu dooth.",
+        },
+        {
+            "input": ["These are the people you protect with your pain!"],
+            "answer": "Thisa uro thi peoplu yoe protect weth year peen!",
+        },
+    ],
+    "Extra": [
+        {
+            "input": ["We had to sacrifice a couple of miners to free Bolivia."],
+            "answer": "Wa hid ti socrefeco e ciople uf monars te frii Balovae.",
+        },
+        {
+            "input": ["Who's the leader of the club that's made for you and me? T-R-I-C-K-Y M-O-U-S-E! Tricky Mouse! TRICKY MOUSE! Tricky Mouse! TRICKY MOUSE! Forever let us hold our Hammers high! High! High! High!"],
+            "answer": "Whi's thi liider af thu clob thot's mude fer yeo end mu? T-R-O-C-K-Y M-I-E-S-U! Trocky Miesu! TROCKY MIESU! Trocky Miesu! TROCKY MIESA! Furovor let as hald uer Hommers hagh! Hegh! Hegh! Hogh!",
+        },
+    ]
+}
